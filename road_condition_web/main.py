a69df04de8094b596cf6bbbacde89f5d3a4885c1
biaheart <44721134+biaheart@users.noreply.github.com>
2020-09-16 23:26:47 +0800

    基本上实现上位机的展示结果功能，只需调整一下显示的图片url接口就可以了

--- road_condition_web/main.py
+++ road_condition_web/main.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, request
-import os, h5py, pickle, re
-
+from flask import Flask,render_template,request
+import os,base64
 
 app = Flask(__name__)
 
@@ -8,22 +7,23 @@ app = Flask(__name__)
 # 默认路由
 @app.route('/')  # 路由默认使用GET方式进行路径访问，可以配置成methods=['GET', 'POST']等
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
-# 初始化路由
-@app.route('/initialization')
-def initialization():
+@app.route('/showPicture', methods=['POST', 'GET'])
+def picture():
+    # if request.method == "POST":
+    #     # 发送图片
+    #     img = open("static/data/eg_tulip.jpg", 'rb')  # 读取图片文件
+    #     data = base64.b64encode(img.read()).decode()  # 进行base64编码
+    #     html = '''<img src="data:image/jpg;base64,{}" style="width:100%;height:100%;"/>'''  # html代码
+    #     htmlstr = html.format(data)  # 添加数据
+    #     return htmlstr
     pass
 
 
-@app.route('/dataPreprocess', methods=['POST'])
-def message():
-    pass
-
-
-@app.route('/get', methods=["GET"])
-def get():
+@app.route('/receivePicture', methods=["GET", "POST"])
+def receivePicture():
     pass
 
 
